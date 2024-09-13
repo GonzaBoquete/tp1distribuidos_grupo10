ec2f5e0d3f92867114f5fdeed3ab7e9f403cd511
daianaVillarreal <daiu.ale.07@gmail.com>
2024-09-12 23:13:47 -0300

    prueba commit

--- src/main/java/com/stockearte/tp1_grupo10/controller/StockController.java
+++ src/main/java/com/stockearte/tp1_grupo10/controller/StockController.java
@@ -1,9 +1,13 @@
 package com.stockearte.tp1_grupo10.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.stockearte.tp1_grupo10.model.Stock;
 import com.stockearte.tp1_grupo10.service.StockService;
 
 @Controller
@@ -12,4 +16,10 @@ public class StockController {
 
 	@Autowired
 	private StockService stockService;
+	
+	@SuppressWarnings("rawtypes")
+	@PostMapping("/add")
+	public ResponseEntity add(@RequestBody Stock stock) {
+		return ResponseEntity.ok(stockService.add(stock));
+	}
 }
